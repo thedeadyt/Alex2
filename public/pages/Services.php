5ecedae8dc1caecffe72ff351baf8b2b12e22331
thedeadyt <sofyalexisleclubdes5@gmail.com>
2025-06-30 16:17:17 +0200

    service

---
+++ public/pages/Services.php
@@ -0,0 +1,31 @@
+<?php
+require_once __DIR__ . '/../../config/config.php';
+include __DIR__ . '/../../includes/header.php';
+// ... le reste
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Page Title</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
+    <script src='main.js'></script>
+</head>
+<body>
+    <div class="bg-black text-green-400 font-mono p-6 rounded-xl shadow-lg max-w-2xl mx-auto space-y-3 text-sm">
+  <div><span class="text-blue-400">&lt;Alex²/&gt;</span> ➜ <span>run branding</span></div>
+
+  <div x-data="{ open: true }" x-show="open" x-transition>
+    <div class="pl-4">> Initialisation du service...</div>
+    <div class="pl-4">> Création d'identités visuelles sur-mesure</div>
+    <div class="pl-4">> Logo, charte graphique, déclinaisons print & web</div>
+    <div class="pl-4">> Statut : <span class="text-green-500">TERMINÉ ✓</span></div>
+  </div>
+</div>
+</body>
+</html>
+<?php
+include __DIR__ . '/../../includes/footer.php';
+?>
